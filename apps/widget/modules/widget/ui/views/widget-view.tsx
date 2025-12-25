b4cbacae3743c2e7c194322ea07f9a6e9e152d48
HafizSyedAhmedAli <hafizsyedahmedali12@gmail.com>
2025-12-25 21:55:54 +0500

    Implemented Widget Loading

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -4,13 +4,18 @@ import { screenAtom } from "@/modules/widget/atoms/widget-atoms";
 import { WidgetAuthScreen } from "@/modules/widget/ui/screens/widget-auth-screen";
 import { WidgetErrorScreen } from "@/modules/widget/ui/screens/widget-error-screen";
 import { useAtomValue } from "jotai";
+import { WidgetLoadingScreen } from "@/modules/widget/ui/screens/widget-loading-screen";
 
-export const WidgetView = ({ organizationId }: { organizationId: string }) => {
+export const WidgetView = ({
+  organizationId,
+}: {
+  organizationId: string | null;
+}) => {
   const screen = useAtomValue(screenAtom);
 
   const screenComponents = {
+    loading: <WidgetLoadingScreen organizationId={organizationId} />,
     error: <WidgetErrorScreen />,
-    loading: <p>TODO: Loading</p>,
     auth: <WidgetAuthScreen />,
     voice: <p>TODO: Voice</p>,
     inbox: <p>TODO: Inbox</p>,
